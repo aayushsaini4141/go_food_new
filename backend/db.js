0547aeb90cd1fc638e60a0a99637f8f682bd5c57
Ayush saini <aayush.saini4141@gmail.com>
2023-07-13 21:31:28 +0530

    global.food_items

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,25 @@
 var mongoose = require('mongoose');
 var mongoURL = 'mongodb+srv://ayush:2022@cluster0.adm8vm0.mongodb.net/gofoodmern'
 
-const mongoDB=async()=>{
-    try{
+const mongoDB = async () => {
+    try {
 
-        await  mongoose.connect(mongoURL);
-        const fetched_data = await mongoose.connection.collection("food_items").find().toArray();
-        console.log();                  //fetched_data
-        console.log('run');
-    }catch (err) {
-        console.log(err);
-    }
-    
+        await mongoose.connect(mongoURL)
+        const fetched_data = await mongoose.connection.collection("food_items");
+            fetched_data.find({})
+            .toArray(function(err, data) { 
+
+                console.log();                  //fetched_data
+                console.log('run');
+                global.food_items = data;
+                console.log(global.food_items);
+            })
+        }
+            catch (err) {
+                console.log(err);
+            }
 }
-  
+
+
 
 module.exports = mongoDB;
