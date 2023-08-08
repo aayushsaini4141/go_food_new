eb54adad6fd6a1be3b33af3503fc7856ffcb8763
Ayush saini <aayush.saini4141@gmail.com>
2023-08-09 02:51:48 +0530

    create login signup in frontend

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ export default function Card(props) {
     <div>
          <div>
         <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-          <img src={props.imgSrc} className="card-img-top" alt="..." />
+          <img src={props.imgSrc} className="card-img-top" alt="..."style={{height:"120px",objectFit:"fill"}}/>
           <div className="card-body">
             <h5 className="card-title">{props.foodName}</h5>
             {/* <p className="card-text">This is something important text.</p> */}
